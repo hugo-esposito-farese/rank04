a26a9357ffd275ac796c5c568e5aabf9c7c3f2cb
hesposit <hesposit@f4r6s12.paris.42.school>
2026-08-07 11:43:26 +0200

    palindrome update

--- exercices/05_palindrome_partitioner/py_palindrome_partitioner.py
+++ exercices/05_palindrome_partitioner/py_palindrome_partitioner.py
@@ -1,27 +1,12 @@
 def palindrome_partitioner(s: str) -> int:
-    n = len(s)
-    if n <= 1:
+    if not s or s == s[::-1]:
         return 0
 
-    is_palindrome = [[False] * n for _ in range(n)]
-    for i in range(n):
-        is_palindrome[i][i] = True
+    mini = len(s)
 
-    for length in range(2, n + 1):
-        for i in range(n - length + 1):
-            j = i + length - 1
-            if s[i] == s[j] and (length == 2 or is_palindrome[i + 1][j - 1]):
-                is_palindrome[i][j] = True
-
-    min_cuts = [0] * n
-    for i in range(n):
-        if is_palindrome[0][i]:
-            min_cuts[i] = 0
-        else:
-            best = i
-            for j in range(i):
-                if is_palindrome[j + 1][i] and min_cuts[j] + 1 < best:
-                    best = min_cuts[j] + 1
-            min_cuts[i] = best
-
-    return min_cuts[n - 1]
+    for i in range(1, len(s)):
+        gauche = s[:i]
+        droite = s[i:]
+        if gauche == gauche [::-1]:
+            mini = min(mini, 1 + palindrome_partitioner(droite))
+    return mini
